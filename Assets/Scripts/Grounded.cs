5bbd19b25887fc702bf5b5364accec7888e46914
fernandrade04 <fernandrade04@hotmail.com>
2022-06-06 22:57:37 -0300

    Menu simples com acessibilidade para deficientes visuais e com deficiencia de audição

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    Personagem Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject.GetComponent<Personagem>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collisor)
+    {
+        if(collisor.gameObject.layer == 3)
+        {
+            Player.isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collisor)
+    {
+        if(collisor.gameObject.layer == 3)
+        {
+            Player.isJumping = true;
+        }
+    }
+}
